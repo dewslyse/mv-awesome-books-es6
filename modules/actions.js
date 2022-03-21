e7d2ec12590c2a777a7376a3e88a9c5d5bcafb34
dewslyse <dewslyse@gmail.com>
2022-03-21 18:14:58 +0000

    Add actions module

---
+++ modules/actions.js
@@ -0,0 +1,46 @@
+import Library from "./library.js";
+
+const addedBooks = document.querySelector('.added-books');
+
+export default class Actions {
+    static displayBooks = () => {
+        const books = Library.retrieveBooks();
+
+        books.forEach((book) => {
+            Actions.addNewBook(book);
+        });
+    }
+
+    static addNewBook = (book) => {
+        const bookContainer = document.createElement('div');
+        bookContainer.classList.add('book');
+        const p = document.createElement('p');
+        p.classList.add('item');
+        p.innerHTML = `
+            "<span class="added-title">${book.title}</span>" by ${book.author}
+        `;
+
+        const removeBtn = document.createElement('button');
+        removeBtn.classList.add('remove-btn');
+        removeBtn.textContent = 'Remove';
+
+        removeBtn.addEventListener('click', () => {
+            Library.removeBook(book.id);
+        });
+
+        bookContainer.appendChild(p);
+        bookContainer.appendChild(removeBtn);
+        addedBooks.appendChild(bookContainer);
+    }
+
+    static deleteBook = (item) => {
+        if (item.classList.contains('remove-btn')) {
+            item.parentElement.remove();
+        }
+    }
+
+    static resetInputs = () => {
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+    }
+}
