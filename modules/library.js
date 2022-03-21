5a146be8fccdeb7b0edcd6eac3332096eed96c65
dewslyse <dewslyse@gmail.com>
2022-03-21 18:20:44 +0000

    Add file to modules directory

--- modules/library.js
+++ modules/library.js
@@ -1,29 +1,29 @@
 export default class Library {
     static retrieveBooks = () => {
-        let books;
-        if (localStorage.getItem('books') === null) {
-            books = [];
-        } else {
-            books = JSON.parse(localStorage.getItem('books'));
-        }
-        return books;
+      let books;
+      if (localStorage.getItem('books') === null) {
+        books = [];
+      } else {
+        books = JSON.parse(localStorage.getItem('books'));
+      }
+      return books;
     }
 
     static addBook = (book) => {
-        const books = Library.retrieveBooks();
-        books.push(book);
-        localStorage.setItem('books', JSON.stringify(books));
+      const books = Library.retrieveBooks();
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
     }
 
     static removeBook = (id) => {
-        const books = Library.retrieveBooks();
+      const books = Library.retrieveBooks();
 
-        books.forEach((book, index) => {
-            if (book.id === id) {
-                books.splice(index, 1);
-            }
-        });
+      books.forEach((book, index) => {
+        if (book.id === id) {
+          books.splice(index, 1);
+        }
+      });
 
-        localStorage.setItem('books', JSON.stringify(books));
+      localStorage.setItem('books', JSON.stringify(books));
     }
 }
